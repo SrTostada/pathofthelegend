e9e12e7faac68bf95d29470963a5fbadc7a9d2b1
SrTostada <81331476+SrTostada@users.noreply.github.com>
2023-04-10 16:20:50 -0600

    update in atacks

--- PathOfTheLegend/Assets/CombateCuerpoaCuerpo.cs
+++ PathOfTheLegend/Assets/CombateCuerpoaCuerpo.cs
@@ -6,9 +6,24 @@ public class CombateCuerpoaCuerpo : MonoBehaviour
 {
     [SerializeField] private Transform controladorGolpe;
 
+    [SerializeField] private Transform controladorGolpeEspecial;
+
     [SerializeField] private float radioGolpe;
 
     [SerializeField] private float dañoGolpe;
+
+    [SerializeField] private float radioGolpeEspecial;
+
+    [SerializeField] private float dañoGolpeEspecial;
+
+    [SerializeField] private float tiempoEntreAtaques;
+
+    [SerializeField] private float tiempoEntreAtaquesEspeciales;
+
+    [SerializeField] private float tiempoSiguienteAtaque;
+
+    [SerializeField] private float tiempoSiguienteAtaqueEspecial;
+
 
     private Animator animator;
 
@@ -19,8 +34,28 @@ public class CombateCuerpoaCuerpo : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        if (tiempoSiguienteAtaque > 0) {
+
+            tiempoSiguienteAtaque -= Time.deltaTime;
+        }
+
+        if (tiempoSiguienteAtaqueEspecial > 0)
+        {
+
+            tiempoSiguienteAtaqueEspecial -= Time.deltaTime;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.K) && tiempoSiguienteAtaque <= 0)
+        {
             Golpe();
+            tiempoSiguienteAtaque = tiempoEntreAtaques;
+        }
+        if (Input.GetKeyDown(KeyCode.L) && tiempoSiguienteAtaqueEspecial <= 0)
+        {
+            GolpeEspecial();
+            tiempoSiguienteAtaqueEspecial = tiempoEntreAtaquesEspeciales;
+        }
     }
 
     private void Golpe() {
@@ -40,10 +75,31 @@ public class CombateCuerpoaCuerpo : MonoBehaviour
 
     }
 
+
+    private void GolpeEspecial()
+    {
+
+        animator.SetTrigger("GolpeEspecial");
+
+        Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpeEspecial.position, radioGolpeEspecial);
+
+        /* foreach (Collider2D colisionador in objetos) {
+
+             if (colisionador.CompareTag("Enemigo")) {
+
+                 colisionador.transform.GetComponent<Enemigo>().TomarDaño(dañoGolpe);
+             }
+
+         }*/
+
+    }
+
     private void OnDrawGizmos()
     {
 
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(controladorGolpeEspecial.position, radioGolpeEspecial);
     }
 }
